1976594e612b304a951d32addbdb9197ca922308
ruslannovik <novik.rv93@gmail.com>
2021-02-01 13:30:14 +0300

    feat: func check()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const stack = [];
+
+    for (let item of str) {
+        for (let conf of bracketsConfig) {
+            if (item === conf[0] && conf[0] !== conf[1]) {
+                stack.push(item);
+            } else if (item === conf[1] && conf[0] !== conf[1]) {
+                const lastItem = stack.pop();
+
+                if (lastItem !== conf[0]) {
+                    return false;
+                }
+            } else if (item === conf[0] && conf[0] === conf[1]) {
+                if (stack[stack.length - 1] !== conf[0]) {
+                    stack.push(item);
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+    }
+
+    return !stack.length;
 }
+
